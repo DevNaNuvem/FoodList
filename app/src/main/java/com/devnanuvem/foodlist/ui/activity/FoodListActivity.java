454f7b4665c2ec471bdeec23603993d27010f62e
devnanuvem <devnanuvem@protonmail.com>
2020-03-14 02:10:34 -0300

    [food-0004] refactoring main activity to food list

--- app/src/main/java/com/devnanuvem/foodlist/ui/activity/FoodListActivity.java
+++ app/src/main/java/com/devnanuvem/foodlist/ui/activity/FoodListActivity.java
@@ -1,4 +1,4 @@
-package com.devnanuvem.foodlist;
+package com.devnanuvem.foodlist.ui.activity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -7,21 +7,23 @@ import android.widget.ListView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.devnanuvem.foodlist.R;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class FoodListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.food_list_activity);
         setTitle("Lista de comidas");
 
         List<String> foodList = new ArrayList<>(Arrays.asList("Lasanha", "Bife à cavalo", "Macarronada", "Parmegianna"));
-        ListView foodListView = findViewById(R.id.activity_main_food_list_view);
+        ListView foodListView = findViewById(R.id.activity_food_list_listview);
 
         foodListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foodList));
     }
